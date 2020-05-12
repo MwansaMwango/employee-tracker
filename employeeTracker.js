8024d2e8271e577f2e73632ee39e575d076054d2
Mwansa <Mwansad.mwango@gmail.com>
2020-05-12 23:20:02 +0800

    Fixed view all employees query

--- employeeTracker.js
+++ employeeTracker.js
@@ -16,194 +16,133 @@ const connection = mysql.createConnection({
   database: "employee_trackerdb"
 });
 
+// Connect to MySQL datbase
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected to database!");
-  runSearch();
-});
 
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "selection",
+});
+    //Get list of departments
+    const queryAllDepts = `SELECT name FROM department`;
+    let deptList = [];
+    connection.query(queryAllDepts, function(err, res) {
+          deptList = res.map(row => row.name);
+
+    })
+let allDepts = '';
+
+
+const queryAllData = 
+      `SELECT 
+        e.id,
+        e.first_name,
+        e.last_name,
+        role.title,
+        department.name as department,
+        role.salary,
+        concat (m.first_name,' ', m.last_name) manager
+      FROM employee e
+      INNER JOIN employee m
+        ON m.id = e.manager_id
+      INNER JOIN role
+        ON e.role_id = role.id 
+      INNER JOIN department
+        ON role.department_id = department.id`;
+
+function promptAction() {
+  return inquirer.prompt({
+      name: "action",
       type: "rawlist",
       message: "What would you like to do?",
       choices: [
         "View all employees",
-        "View all employees by Department",
-        "View all employees by Manager",
+          "View all employees by Department",
+          "View all employees by Manager",
+        "View department total salary",
         "Add Employee",
         "Remove Employee",
         "Update Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View department total salary"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.selection) {
+          "Update Employee Role",
+          "Update Employee Manager"
+      ]}
+  )
+}
+
+async function getAction () {
+  try {
+
+    const promptActionAnswer = await promptAction();
+ 
+    switch (promptActionAnswer.action) {
+    
       case "View all employees":
         viewEmployees();
         break;
-
+    
       case "View all employees by Department":
-        viewEmployeesByDept();
-        break;
+        const selectedDept = await inquirer.prompt({
+          name: "department",
+          type: "rawlist",
+          message: "What department would you like to view?",
+          choices: deptList
+        }).department
+        viewEmployeesByDept( await selectedDept);
 
-      case "View all employees by Manager":
-        viewEmployeesByManager();
-        break;
-
-      case "Add Employee":
-        addEmployee();
-        break;
-
-      case "Remove Employee":
-        removeEmployee();
-        break;
-
-      case "Update Employee":
-        updateEmployee();
-        break;
-      
-      case "Update Employee Role":
-        updateEmployeeRole();
-        break; 
-
-      case "Update Employee Manager":
-        updateEmployeeManager();
-        break; 
-      case "View department total salary":
-        viewDeptSalaryTotal();
-        break; 
+      break;
     }
-    });
-}
 
-function viewEmployees() {
-
-      const query = "SELECT * FROM employee";
-      connection.query(query, function(err, res) {
-        // for (let i = 0; i < res.length; i++) {
-          console.table(res)
-        // }
-        runSearch();
+  } catch (err) {
+    console.error(err.message);
+  }
   
-    });
 }
-
-function multiSearch() {
-  const query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (let i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
+          
+function viewEmployees() {
+  connection.query(
+    `SELECT 
+      e.id,
+      e.first_name,
+      e.last_name,
+      role.title,
+      department.name as department,
+      role.salary,
+      concat (m.first_name,' ', m.last_name) manager
+    FROM employee e
+    INNER JOIN employee m
+      ON m.id = e.manager_id
+    INNER JOIN role
+      ON e.role_id = role.id 
+    INNER JOIN department
+      ON role.department_id = department.id`, function(err, res) {
+    console.table(res);
+    getAction();
   });
 }
 
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      const query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (let i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
+function viewEmployeesByDept(selectedDept) {
+   console.log('Variable for selected department'+ selectedDept);
+  // connection.query(`SELECT 
+  //   e.id,
+  //   e.first_name,
+  //   e.last_name,
+  //   role.title,
+  //   department.name as department,
+  //   role.salary,
+  //   concat (m.first_name,' ', m.last_name) manager
+  // FROM employee e
+  // INNER JOIN employee m
+  //   ON m.id = e.manager_id
+  // INNER JOIN role
+  //   ON e.role_id = role.id 
+  // INNER JOIN department
+  //   ON role.department_id = department.id
+  //   WHERE department.name = '${selectedDept}'` , function(err, res) {
+  //   console.table(res);
+    // getAction();
+  // });
 }
 
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
-    });
-}
+getAction();
 
-function songAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      const query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
 
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
-        console.log(res.length + " matches found!");
-        for (let i = 0; i < res.length; i++) {
-          console.log(
-            i+1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
-          );
-        }
 
-        runSearch();
-      });
-    });
-}
