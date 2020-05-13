e92515a57bd5fcc60db8cb8e54fd71de05571701
Mwansa <Mwansad.mwango@gmail.com>
2020-05-13 22:17:14 +0800

    Fixed view employees by manager

--- employeeTracker.js
+++ employeeTracker.js
@@ -29,6 +29,26 @@ connection.connect(function(err) {
           deptList = res.map(row => row.name);
 
     })
+
+    //Get list of managers
+    const queryAllMgrs = 
+    `SELECT 
+    e.manager_id,
+    concat (m.first_name,' ', m.last_name) manager
+  FROM employee e
+  INNER JOIN employee m
+    ON m.id = e.manager_id
+  INNER JOIN role
+    ON e.role_id = role.id 
+  INNER JOIN department
+    ON role.department_id = department.id`;
+    let managerList = [];
+    let managerObjArr = [];
+    connection.query(queryAllMgrs, function(err, res) {
+      managerObjArr = res;
+      managerList = res.map(row => row.manager);
+    })
+    
 let allDepts = '';
 
 
@@ -80,17 +100,29 @@ async function getAction () {
         break;
     
       case "View all employees by Department":
-        const answer = await inquirer.prompt({
+        const promptDept = await inquirer.prompt({
           name: "department",
           type: "rawlist",
           message: "What department would you like to view?",
           choices: deptList
         })
-        const selectedDept = answer.department
-        console.log(selectedDept);
+        const selectedDept = promptDept.department
         viewEmployeesByDept(selectedDept);
+        break;
 
-      break;
+      case "View all employees by Manager":
+        const promptMgr = await inquirer.prompt({
+          name: "manager",
+          type: "rawlist",
+          message: "Under which manager are the employees?",
+          choices: managerList
+        })
+        const selectedMgr = promptMgr.manager
+        const selectedMgrObj = await managerObjArr.find(row => row.manager === selectedMgr)
+        const selectedMgrId = selectedMgrObj.manager_id;
+        viewEmployeesByMgr(selectedMgrId);
+        break;
+        
     }
 
   } catch (err) {
@@ -122,11 +154,12 @@ function viewEmployees() {
 }
 
 function viewEmployeesByDept(dept) {
-   console.log('Variable for selected department = '+ dept);
+  console.log('Selected department = '+ dept);
   connection.query(`SELECT 
     e.id,
     e.first_name,
     e.last_name,
+    e.manager_id,
     role.title,
     department.name as department,
     role.salary,
@@ -138,9 +171,33 @@ function viewEmployeesByDept(dept) {
     ON e.role_id = role.id 
   INNER JOIN department
     ON role.department_id = department.id
-    WHERE department.name = '${dept}'` , function(err, res) {
-    console.table(res);
-    getAction();
+  WHERE department.name = '${dept}'` , function(err, res) {
+  console.table(res);
+  getAction();
+  });
+}
+
+function viewEmployeesByMgr(mgrId) { 
+  console.log('Selected manager = '+ mgrId);
+  connection.query(`SELECT 
+    e.id,
+    e.first_name,
+    e.last_name,
+    e.manager_id,
+    role.title,
+    department.name as department,
+    role.salary,
+    concat (m.first_name,' ', m.last_name) manager
+  FROM employee e
+  LEFT JOIN employee m
+    ON m.id = e.manager_id
+  INNER JOIN role
+    ON e.role_id = role.id 
+  INNER JOIN department
+    ON role.department_id = department.id
+  WHERE e.manager_id = '${mgrId}'` , function(err, res) {
+  console.table(res);
+  getAction();
   });
 }
 
