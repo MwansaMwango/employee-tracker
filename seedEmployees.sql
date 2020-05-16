3d69c838a8151f69482f864bbe78b97c23cf1b45
Mwansa <Mwansad.mwango@gmail.com>
2020-05-16 10:13:32 +0800

    Updated seed data

--- seedEmployees.sql
+++ seedEmployees.sql
@@ -4,11 +4,11 @@ LIMIT 0, 1000
 
 -- Date: 2020-05-16 09:15
 */
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (16,'Mick','Chani',1,17);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (17,'Ashley','Rodriguez',3,NULL);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (18,'Shortcut','Sam',2,17);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (19,'Malia','Brown',5,NULL);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (20,'Sarah','Lourd',6,NULL);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (21,'Tom','Allen',7,20);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (22,'Tammer','Galal',4,18);
-INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (28,'Luke','JS',4,18);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Mick','Chani',1,17);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Joe','Exotic',3,NULL);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Shortcut','Sam',2,17);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Malia','Brown',5,NULL);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Sarah','Lourd',6,NULL);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Tom','Allen',7,20);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Tammer','Galal',4,18);
+INSERT INTO `employee` (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES ('Luke','JS',4,18);
