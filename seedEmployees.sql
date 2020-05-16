8defafc8d03703b709b279d486e4350490a0af44
Mwansa <Mwansad.mwango@gmail.com>
2020-05-16 09:17:31 +0800

    Initial seed commit with test employee data

---
+++ seedEmployees.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM employee_trackerdb.employee
+LIMIT 0, 1000
+
+-- Date: 2020-05-16 09:15
+*/
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (16,'Mick','Chani',1,17);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (17,'Ashley','Rodriguez',3,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (18,'Shortcut','Sam',2,17);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (19,'Malia','Brown',5,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (20,'Sarah','Lourd',6,NULL);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (21,'Tom','Allen',7,20);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (22,'Tammer','Galal',4,18);
+INSERT INTO `` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (28,'Luke','JS',4,18);
